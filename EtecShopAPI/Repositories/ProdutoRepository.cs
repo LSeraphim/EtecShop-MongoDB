82c21a872e899086b8d974ff5deeea8f487c0c9e
LSeraphim <leostart133@gmail.com>
2024-09-13 20:32:25 -0300

    criado metodos da API, GET, PUT,  POST, DELETE

---
+++ EtecShopAPI/Repositories/ProdutoRepository.cs
@@ -0,0 +1,30 @@
+
+using EtecShopAPI.Collections;
+using MongoDB.Driver;
+
+namespace EtecShopAPI.Repositories;
+public class ProdutoRepository : IProdutoRepository
+{
+    private readonly IMongoCollection<Produto> _collection;
+    public ProdutoRepository(IMongoDatabase mongoDatabase)
+    {
+       _collection = mongoDatabase.GetCollection<Produto>("produtos"); 
+    }
+
+    public async Task CreateAsync(Produto produto) => 
+        await _collection.InsertOneAsync(produto);
+
+    public async Task DeleteAsync(string id) =>
+        await _collection.DeleteOneAsync(p => p.Id == id);
+    
+
+    public async Task<List<Produto>> GetAllAsync() => 
+        await _collection.Find(_ => true).ToListAsync();
+    
+
+    public Task<Produto> GetByIdAsync(string id) =>
+        _collection.Find(_ => _.Id == id).FirstOrDefaultAsync();
+
+    public async Task UpdateAsync(Produto produto) =>
+        await _collection.ReplaceOneAsync(_ => _.Id == produto.Id, produto);
+}
